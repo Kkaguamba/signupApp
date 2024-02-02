18abe89e6fa7ec5d9b0f6f9c9c225331555af5da
Kkaguamba <kkaguamba@gmail.com>
2024-02-02 16:05:47 +0300

    SignupApp

---
+++ app/src/main/java/com/example/mysignupapp/NewUser.java
@@ -0,0 +1,55 @@
+package com.example.mysignupapp;
+
+public class NewUser {
+    private String fName;
+    private String lName;
+    private String email;
+    private String phone;
+
+    private String passwd;
+
+    public NewUser() {
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+
+}
--- app/src/main/java/com/example/mysignupapp/RegisterActivity.java
+++ app/src/main/java/com/example/mysignupapp/RegisterActivity.java
@@ -1,13 +1,29 @@
 package com.example.mysignupapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edtfName, edtlName, edtEmail, edtPhone, edtPasswd, edtconfPasswd;
+    Button btnRegister;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    NewUser newUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +31,66 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         TextView btn=findViewById(R.id.signin);
+        edtfName = findViewById(R.id.fname);
+        edtlName = findViewById(R.id.lname);
+        edtEmail = findViewById(R.id.email);
+        edtPhone = findViewById(R.id.phone);
+        edtPasswd = findViewById(R.id.pass);
+        edtconfPasswd = findViewById(R.id.confpass);
+        btnRegister = findViewById(R.id.register);
+
+        // Write a message to the database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("user info");
+        newUser = new NewUser();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pass = edtPasswd.getText().toString();
+                String confPass = edtconfPasswd.getText().toString();
+                String fName = edtfName.getText().toString();
+                String lName = edtlName.getText().toString();
+                String Email = edtEmail.getText().toString();
+                String Phone = edtPhone.getText().toString();
+
+                if (fName.isEmpty() || lName.isEmpty() || Email.isEmpty() || Phone.isEmpty()
+                        || pass.isEmpty() || confPass.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Fill in the fields", Toast.LENGTH_LONG).show();
+                }
+
+                if (pass.equals(confPass)){
+                    addNewUser(fName, lName, Email, Phone, pass);
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Password do not match.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         btn.setOnClickListener(v -> startActivity(new Intent(RegisterActivity.this,LoginActivity.class)));
+    }
+    public void addNewUser(String fName, String lName, String Email, String Phone, String Passwd){
+        newUser.setfName(fName);
+        newUser.setlName(lName);
+        newUser.setEmail(Email);
+        newUser.setPhone(Phone);
+        newUser.setPasswd(Passwd);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(newUser);
+                Toast.makeText(getApplicationContext(),"User registered successfully!",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(),"Error! user registration failed!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
